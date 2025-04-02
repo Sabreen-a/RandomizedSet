3e8da6d823d8f75c61b5623bd78cddfcb636ecbc
sabre <sabreenashraf22894@gmail.com>
2025-04-02 05:22:37 +0200

    Inint Randomized App

---
+++ RandomizedSet.App/RandomizedSet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomizedSet.App
+{
+  
+
+    public class RandomizedSet
+    {
+        private List<int> values; 
+        private Dictionary<int, int> valToIndex; 
+        private Random random;
+
+        public RandomizedSet()
+        {
+            values = new List<int>();
+            valToIndex = new Dictionary<int, int>();
+            random = new Random();
+        }
+
+        public bool Insert(int val)
+        {
+            if (valToIndex.ContainsKey(val))
+                return false; 
+
+            valToIndex[val] = values.Count; 
+            values.Add(val); 
+            return true;
+        }
+
+        public bool Remove(int val)
+        {
+            if (!valToIndex.ContainsKey(val))
+                return false;
+
+            int index = valToIndex[val]; 
+
+            values.RemoveAt(index); 
+
+           
+            valToIndex.Remove(val);
+
+          
+            for (int i = index; i < values.Count; i++)
+            {
+                valToIndex[values[i]] = i; 
+            }
+
+            return true;
+        }
+
+        public int GetRandom()
+        {
+            int randomIndex = random.Next(values.Count);
+            return values[randomIndex];
+        }
+    }
+
+}
---
+++ RandomizedSet/RandomizedSetTests.cs
@@ -0,0 +1,41 @@
+﻿using RandomizedSetApp;
+using System;
+using Xunit;
+
+public class RandomizedSetTests//we can run it from view=>test explorer=>run
+{
+    [Fact]
+    public void TestInsert()
+    {
+        RandomizedSet randomizedSet = new RandomizedSet();
+
+        Assert.True(randomizedSet.Insert(10)); 
+        Assert.True(randomizedSet.Insert(20)); 
+        Assert.False(randomizedSet.Insert(10)); 
+    }
+
+    [Fact]
+    public void TestRemove()
+    {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        randomizedSet.Insert(10);
+        randomizedSet.Insert(20);
+        randomizedSet.Insert(30);
+
+        Assert.True(randomizedSet.Remove(20));
+        Assert.False(randomizedSet.Remove(20)); 
+        Assert.True(randomizedSet.Remove(10)); 
+    }
+
+    [Fact]
+    public void TestGetRandom()
+    {
+        RandomizedSet randomizedSet = new RandomizedSet();
+        randomizedSet.Insert(10);
+        randomizedSet.Insert(20);
+        randomizedSet.Insert(30);
+
+        int randomValue = randomizedSet.GetRandom();
+        Assert.Contains(randomValue, new int[] { 10, 20, 30 }); 
+    }
+}
